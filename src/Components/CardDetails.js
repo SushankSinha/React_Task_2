559ab9eff4a35eb77901555b05ba7e0d1f756058
SushankSinha <116825226+SushankSinha@users.noreply.github.com>
2023-07-13 22:34:56 +0530

    Update CardDetails.js

--- src/Components/CardDetails.js
+++ src/Components/CardDetails.js
@@ -2,42 +2,42 @@ const CardDetails = [
   {
     imgURL:
       "https://rukminim1.flixcart.com/image/200/200/xif0q/instant-camera/y/l/3/mini-12-instax-16791340-fujifilm-original-imagzkbhzrx7ssyd.jpeg?q=70",
-    name: "Pinstax Camera",
+    name: "Instax Camera",
     price: "Rs. 3999.00",
     rating: "⭐⭐⭐⭐⭐"
   },
   {
     imgURL:
       "https://rukminim1.flixcart.com/image/612/612/xif0q/shoe/l/x/3/7-378513-puma-navy-silver-original-imaggqcs8kzzmzcn.jpeg?q=70",
-    name: "Ruma shoes",
+    name: "Puma shoes",
     price: "Rs. 1999.00",
     rating: "⭐⭐⭐"
   },
   {
     imgURL:
       "https://rukminim1.flixcart.com/image/612/612/xif0q/t-shirt/k/k/l/s-t9-blkwh-teemex-original-imaghmrfhg9sejdr.jpeg?q=70",
-    name: "Abidas T-shirt",
+    name: "Adidas T-shirt",
     price: "Rs. 799.00",
     rating: "⭐⭐⭐⭐"
   },
   {
     imgURL:
       "https://rukminim1.flixcart.com/image/200/200/xif0q/mobile/i/8/5/-original-imagmmmhmjpfvbry.jpeg?q=70",
-    name: "Motokola E13",
+    name: "Motorola E13",
     price: "Rs. 7499.00",
     rating: "⭐⭐⭐⭐"
   },
   {
     imgURL:
       "https://rukminim1.flixcart.com/image/200/200/xif0q/backpack/q/u/e/-original-imaghzzunvhatss4.jpeg?q=70",
-    name: "Pithoo",
+    name: "Bagpack",
     price: "Rs. 1499.00",
     rating: "⭐⭐⭐⭐"
   },
   {
     imgURL:
       "https://rukminim1.flixcart.com/image/200/200/xif0q/smartwatch/v/l/r/-original-imagzbtfssc8anxw.jpeg?q=70",
-    name: "Bewakoof Watch",
+    name: "Smart Watch",
     price: "Rs. 4999.00",
     rating: "⭐⭐⭐⭐"
   }
